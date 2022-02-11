974b46b5b5ddd6be305be74f67ca1e0e054ad262
wagnerkaba <bom.link+github@gmail.com>
2022-02-11 15:54:09 -0300

    exercicios semana 7

---
+++ semana 7/type-number.js
@@ -0,0 +1,18 @@
+const primeiroNumero =1;
+const segundoNumero = 2;
+const adicao = primeiroNumero + segundoNumero;
+console.log(adicao);
+
+// Divisão por zero não é um erro em JavaScript: ele simplesmente retorna “Infinity”. 
+console.log(primeiroNumero / 0);
+
+//No entanto, há uma exceção: zero dividido por zero não tem um valor bem definido e o resultado dessa operação é o valor especial não numérico NaN.
+console.log(0/0);
+
+//unicode
+const tique = '\u2705';
+const hiragana = '\u3041';
+console.log(hiragana);
+console.log(tique);
+console.log('\u1F235');
+
