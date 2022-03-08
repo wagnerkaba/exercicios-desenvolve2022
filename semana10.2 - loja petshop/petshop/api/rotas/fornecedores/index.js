da3d7d3577c703c8cb78f04ffb803e10b13d0f7b
wagnerkaba <bom.link+github@gmail.com>
2022-03-08 16:11:33 -0300

    exercicios semana 10

--- semana10.2 - loja petshop/petshop/api/rotas/fornecedores/index.js
+++ semana10.2 - loja petshop/petshop/api/rotas/fornecedores/index.js
@@ -1,14 +1,24 @@
 const roteador = require('express').Router();
 const TabelaFornecedor = require('./TabelaFornecedor');
+const Fornecedor = require('./Fornecedor');
 
-roteador.use('/', async (requisicao, resposta) => {
-    const resultados = await TabelaFornecedor.listar();
+roteador.get('/', async (requisicao, resposta) => {
+    const resultados = await TabelaFornecedor.listar()
     resposta.send(
         JSON.stringify(resultados)
     );
 
-}
-)
+})
+
+roteador.post('/', async (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body;
+    const fornecedor = new Fornecedor(dadosRecebidos);
+    await fornecedor.criar();
+    resposta.send(
+            JSON.stringify(fornecedor)
+    );
+
+})
 
 module.exports = roteador;
 
