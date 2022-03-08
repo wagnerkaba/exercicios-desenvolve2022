41c37e40ff088db5800aa0e25a877a63e1879f2a
wagnerkaba <bom.link+github@gmail.com>
2022-03-08 17:46:28 -0300

    implmentacao método pegar por id

--- semana10.2 - loja petshop/petshop/api/rotas/fornecedores/index.js
+++ semana10.2 - loja petshop/petshop/api/rotas/fornecedores/index.js
@@ -15,9 +15,27 @@ roteador.post('/', async (requisicao, resposta) => {
     const fornecedor = new Fornecedor(dadosRecebidos);
     await fornecedor.criar();
     resposta.send(
-            JSON.stringify(fornecedor)
+        JSON.stringify(fornecedor)
     );
 
+})
+
+
+roteador.get('/:idFornecedor', async (requisicao, resposta) => {
+
+
+    try {
+        const id = requisicao.params.idFornecedor;
+        const fornecedor = new Fornecedor({ id: id });
+        await fornecedor.carregar();
+        resposta.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch (erro) {
+        resposta.send(JSON.stringify({
+            mensagem: erro.message
+        }))
+    }
 })
 
 module.exports = roteador;
