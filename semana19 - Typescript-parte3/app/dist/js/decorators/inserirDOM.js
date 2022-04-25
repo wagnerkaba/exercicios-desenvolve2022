6480b5209c4b7b57633583dcb2c88a290d7e0d94
wagnerkaba <bom.link+github@gmail.com>
2022-04-25 17:31:29 -0300

    refatoração @inserirDOM

--- semana19 - Typescript-parte3/app/dist/js/decorators/inserirDOM.js
+++ semana19 - Typescript-parte3/app/dist/js/decorators/inserirDOM.js
@@ -1,9 +1,12 @@
 export function inserirDOM(seletor) {
     return function (target, propertyKey) {
         console.log(`@inserirDOM - Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`@inserirDOM - Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+            if (!elemento) {
+                console.log(`@inserirDOM - Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+                elemento = document.querySelector(seletor);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
--- semana19 - Typescript-parte3/app/src/decorators/inserirDOM.ts
+++ semana19 - Typescript-parte3/app/src/decorators/inserirDOM.ts
@@ -1,25 +1,37 @@
 
 // Decorator para pegar um elemento dom e inseri-lo em uma variavel input
 // vide comentários na classe NegociacaoController
-export function inserirDOM(seletor: string){
-    return function(target: any, propertyKey: string){
+export function inserirDOM(seletor: string) {
+    return function (target: any, propertyKey: string) {
 
         // Este decorator cria getters para inputData, inputQuantidade e inputValor
         // Quando alguém invoca esses getters, o retorno é um elemento DOM correspondente
-        
+
         console.log(`@inserirDOM - Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
 
+        let elemento: HTMLElement;
 
-        const getter = function(){
-            const elemento = document.querySelector(seletor);
-            console.log(`@inserirDOM - Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+        const getter = function () {
+
+            
+            if (!elemento) {
+
+                // se o getter for chamado pela primeira vez, então "elemento" vai ser undefined (ou seja, falsy)
+                // nesse caso, o getter vai buscar o elemento dom desejado
+                
+                console.log(`@inserirDOM - Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+
+                elemento = <HTMLElement>document.querySelector(seletor);
+            }
+
+            // se o getter já foi chamado anteriormente, então "elemento" vai conter o elemento dom. Nesse caso, basta retorná-lo.
             return elemento;
         }
 
         Object.defineProperty(
-            target, 
-            propertyKey, 
-            { get: getter}
+            target,
+            propertyKey,
+            { get: getter }
         )
     }
 }
