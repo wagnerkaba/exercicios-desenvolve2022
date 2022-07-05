6f244a59f3d8063f8456fc094ee8f639f4e75730
wagnerkaba <bom.link+github@gmail.com>
2022-07-05 15:13:20 -0300

    exercicios

--- semana26.2 - nodejs JWT/projeto/src/usuarios/usuarios-modelo.js
+++ semana26.2 - nodejs JWT/projeto/src/usuarios/usuarios-modelo.js
@@ -1,13 +1,14 @@
 const usuariosDao = require('./usuarios-dao');
 const { InvalidArgumentError } = require('../erros');
 const validacoes = require('../validacoes-comuns');
+const bcrypt = require('bcrypt');
 
 class Usuario {
   constructor(usuario) {
     this.id = usuario.id;
     this.nome = usuario.nome;
     this.email = usuario.email;
-    this.senha = usuario.senha;
+    this.senhaHash = usuario.senhaHash;
 
     this.valida();
   }
@@ -20,12 +21,23 @@ class Usuario {
     return usuariosDao.adiciona(this);
   }
 
+  async adicionaSenha(senha){
+
+
+    // faz a validação da senha recebida
+    validacoes.campoStringNaoNulo(senha, 'senha');
+    validacoes.campoTamanhoMinimo(senha, 'senha', 8);
+    validacoes.campoTamanhoMaximo(senha, 'senha', 64);
+
+    // transforma a senha em senha hash
+    // armazena a senha hash em vez de armazenar a senha
+    this.senhaHash = await Usuario.gerarSenhaHash(senha);
+  }
+
   valida() {
     validacoes.campoStringNaoNulo(this.nome, 'nome');
     validacoes.campoStringNaoNulo(this.email, 'email');
-    validacoes.campoStringNaoNulo(this.senha, 'senha');
-    validacoes.campoTamanhoMinimo(this.senha, 'senha', 8);
-    validacoes.campoTamanhoMaximo(this.senha, 'senha', 64);
+
   }
 
   
@@ -54,6 +66,14 @@ class Usuario {
   static lista() {
     return usuariosDao.lista();
   }
+
+  //Função para receber a senha e devolver o hash da senha
+  static gerarSenhaHash(senha){
+    const custoHash = 12;
+    return bcrypt.hash(senha, custoHash);
+
+  }
+
 }
 
 module.exports = Usuario;
