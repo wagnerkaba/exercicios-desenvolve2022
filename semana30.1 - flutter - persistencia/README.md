fbc448b585fbf52027744e804ab2a1a35bb8c191
wagnerkaba <bom.link+github@gmail.com>
2022-08-25 15:13:09 -0300

    exercicios

---
+++ semana30.1 - flutter - persistencia/README.md
@@ -0,0 +1,15 @@
+# ByteBank - Um projeto em Flutter
+
+[Proposta de implementação](./implementation-proposal-bytebank-app.pdf)
+
+## Tópicos importantes
+
+
+
+
+
+## Tecnologias utilizadas
+
+* Flutter
+
+
--- semana30.1 - flutter - persistencia/byte_bank/lib/main.dart
+++ semana30.1 - flutter - persistencia/byte_bank/lib/main.dart
@@ -8,10 +8,69 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Dashboard'),),
-      )
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.green,
+          )
+              .copyWith(
+            primary: Colors.green[900],
+            secondary: Colors.blueAccent[700],
+          ),
+          buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blueAccent[700],
+              textTheme: ButtonTextTheme.primary
+          ),
+        ),
+        home: Dashboard(),
     );
   }
+}
 
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                padding: EdgeInsets.all(8.0),
+                height: 100,
+                width: 150,
+                color: Theme.of(context).primaryColorDark,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Icon(
+                      Icons.people,
+                      color: Colors.white,
+                      size: 32.0,
+                    ),
+                    Text(
+                      'Contacts',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                )),
+          )
+        ],
+      ),
+    );
+  }
 }
