38bb2251a23021d859bd47f886c11c0890f8b261
wagnerkaba <bom.link+github@gmail.com>
2022-09-03 13:20:28 -0300

    refatoração para utilização de async await

--- semana30.1 - flutter - persistencia/byte_bank/lib/database/app_database.dart
+++ semana30.1 - flutter - persistencia/byte_bank/lib/database/app_database.dart
@@ -2,46 +2,77 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import '../models/Contact.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then(
-    (dbPath) {
-      final String path = join(dbPath, 'bytebank.db');
-      return openDatabase(
-        path,
-        onCreate: (db, version) {
-          db.execute('CREATE TABLE contacts('
-              'id INTEGER PRIMARY KEY, '
-              'name TEXT, '
-              'account_number INTEGER)');
-        },
-        version: 1,
-        //gambiarra ensinada na aula 5.1 para limpar o banco de dados
-        //fazer upgrade do banco de dados e depois fazer downgrade e ativar opção para apagar bd no downgrade (vide linha abaixo)
-        //onDowngrade: onDatabaseDowngradeDelete,
-      );
+Future<Database> getDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute('CREATE TABLE contacts('
+          'id INTEGER PRIMARY KEY, '
+          'name TEXT, '
+          'account_number INTEGER)');
     },
+    version: 1,
   );
+  // ==== O código abaixo foi refatorado para utilizar async await =====
+  // return getDatabasesPath().then(
+  //   (dbPath) {
+  //     final String path = join(dbPath, 'bytebank.db');
+  //     return openDatabase(
+  //       path,
+  //       onCreate: (db, version) {
+  //         db.execute('CREATE TABLE contacts('
+  //             'id INTEGER PRIMARY KEY, '
+  //             'name TEXT, '
+  //             'account_number INTEGER)');
+  //       },
+  //       version: 1,
+  //       //gambiarra ensinada na aula 5.1 para limpar o banco de dados
+  //       //fazer upgrade do banco de dados e depois fazer downgrade e ativar opção para apagar bd no downgrade (vide linha abaixo)
+  //       //onDowngrade: onDatabaseDowngradeDelete,
+  //     );
+  //   },
+  // );
 }
 
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contactMap = Map();
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
-    return db.insert('contacts', contactMap);
-  });
+Future<int> save(Contact contact) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contactMap = Map();
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
+  return db.insert('contacts', contactMap);
+
+  // ==== O código abaixo foi refatorado para utilizar async await =====
+  // return createDatabase().then((db) {
+  //   final Map<String, dynamic> contactMap = Map();
+  //   contactMap['name'] = contact.name;
+  //   contactMap['account_number'] = contact.accountNumber;
+  //   return db.insert('contacts', contactMap);
+  // });
 }
 
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = [];
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact =
-            Contact(map['id'], map['name'], map['account_number']);
-        contacts.add(contact);
-      }
-      return contacts;
-    });
-  });
+Future<List<Contact>> findAll() async {
+  final List<Contact> contacts = [];
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+  for (Map<String, dynamic> row in result) {
+    final Contact contact =
+        Contact(row['id'], row['name'], row['account_number']);
+    contacts.add(contact);
+  }
+  return contacts;
+
+  // ==== O código abaixo foi refatorado para utilizar async await =====
+  // return getDatabase().then((db) {
+  //   return db.query('contacts').then((maps) {
+  //     final List<Contact> contacts = [];
+  //     for (Map<String, dynamic> row in maps) {
+  //       final Contact contact =
+  //           Contact(row['id'], row['name'], row['account_number']);
+  //       contacts.add(contact);
+  //     }
+  //     return contacts;
+  //   });
+  // });
 }
