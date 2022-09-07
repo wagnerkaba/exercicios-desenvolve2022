47d66f91277d498c44e7d6af6cefcd43ed5067ce
wagnerkaba <bom.link+github@gmail.com>
2022-09-07 16:22:02 -0300

    exercicios até aula 04.1

--- semana30.2 - flutter - web api/byte_bank/lib/http/webclient.dart
+++ semana30.2 - flutter - web api/byte_bank/lib/http/webclient.dart
@@ -26,15 +26,18 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+
+Uri baseUrl = Uri.http('192.168.15.8:8080', 'transactions');
+
 Future<List<Transaction>> findAll() async {
-  Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
   // obs: o celular não consegue acessar localhost:8080/transactions (esse endereço só dá para acessar no próprio computador).
   // é preciso descobrir a url que o celular consegue acessar digitando o comando "ipconfig" no power shell do windows
   // descobri que consigo acessar a rota "transactions", através do endereço: http://192.168.15.8:8080/transactions
   // O endereço 192.168.15.8:8080 estava em IPv4 dentro de Wireless LAN adapter wi-fi
   final Response response =
-      await client.get(Uri.http('192.168.15.8:8080', 'transactions')).timeout(Duration(seconds: 5));
+      await client.get(baseUrl).timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = [];
   for (Map<String, dynamic> transactionJson in decodedJson) {
@@ -45,9 +48,46 @@ Future<List<Transaction>> findAll() async {
           0,
           contactJson['name'],
           contactJson['accountNumber'],
-        ));
+        ),
+    );
     transactions.add(transaction);
   }
   print('decoded json: $decodedJson');
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async{
+
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+  final Response response = await client.post(
+      baseUrl,
+      headers: {
+        'Content-type': 'application/json',
+        'password': '1000',
+      },
+    body: transactionJson
+  );
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
+
+
+
 }
--- semana30.2 - flutter - web api/byte_bank/lib/main.dart
+++ semana30.2 - flutter - web api/byte_bank/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:byte_bank/http/webclient.dart';
+import 'package:byte_bank/models/transaction.dart';
 import 'package:byte_bank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
+import 'models/Contact.dart';
+
 void main() {
   runApp(ByteBankApp());
-  findAll().then((transactions) => print('new transactions $transactions'));
+
+  // linha abaixo tem a finalidade de testar a função save
+  save(Transaction(666.0, Contact(0,'Barnabé',1697))).then((transaction) => print(transaction));
 }
 
 class ByteBankApp extends StatelessWidget {
