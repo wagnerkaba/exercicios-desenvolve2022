daa36b418f4bfa7434e72b57fcea12ba32a8359a
wagnerkaba <bom.link+github@gmail.com>
2022-10-20 10:22:18 -0300

    exercicios até aula 2.08

--- semana34.1 - flutter - testes/byte_bank/test/dashboard_widget_test.dart
+++ semana34.1 - flutter - testes/byte_bank/test/dashboard_widget_test.dart
@@ -1,23 +1,51 @@
-
 import 'package:byte_bank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main(){
-  testWidgets('Should display the main image when Dashboard is opened', (WidgetTester tester) async{
+void main() {
+  testWidgets('Should display the main image when Dashboard is opened',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
     final mainImage = find.byType(Image);
     expect(mainImage, findsOneWidget);
   });
 
-  testWidgets('Should display the first feature when Dashboard is opened', (tester) async {
+  testWidgets(
+      'Should display the transfer feature when Dashboard is opened (maneira de testar #1)',
+      (tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
     // verifica se o icone do feature é monetization_on
-    final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
-    expect(iconTransferFeatureItem,findsOneWidget);
+    final iconTransferFeatureItem =
+        find.widgetWithIcon(FeatureItem, Icons.monetization_on);
+    expect(iconTransferFeatureItem, findsOneWidget);
     // verifica se o texto do item é "Transfer"
-    final nameTransferFeatureItem = find.widgetWithText(FeatureItem, "Transfer");
+    final nameTransferFeatureItem =
+        find.widgetWithText(FeatureItem, "Transfer");
     expect(nameTransferFeatureItem, findsOneWidget);
   });
 
+  testWidgets(
+      'Should display the transfer feature when Dashboard is opened (maneira de testar #2)',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final transferFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+    expect(transferFeatureItem, findsOneWidget);
+  });
+
+  testWidgets(
+      'Should display the transaction feed feature when Dashboard is opened',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final transactionFeedFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transaction Feed', Icons.description));
+    expect(transactionFeedFeatureItem, findsOneWidget);
+  });
+}
+
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if (widget is FeatureItem) {
+    return widget.name == name && widget.icon == icon;
+  }
+  return false;
 }
