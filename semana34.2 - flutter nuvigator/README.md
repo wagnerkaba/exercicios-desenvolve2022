3ceb372ddf8fa2e27c1b5374a45c31525e6262b0
wagnerkaba <bom.link+github@gmail.com>
2022-11-09 09:15:10 -0300

    exercicios

---
+++ semana34.2 - flutter nuvigator/README.md
@@ -0,0 +1,7 @@
+
+
+
+
+Nuvigator v1 - Implementação do nuvigator até aula 2.03
+
+Nuvigator v2 - Refatoração do Nuvigator
--- semana34.2 - flutter nuvigator/nuvigator/android/app/src/main/kotlin/com/example/ecommerceorgs/proj/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ecommerceorgs.proj
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
